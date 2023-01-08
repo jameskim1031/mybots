0a0cdb6894822e26d7fa3167528468ae80fd4e70
jameskim1031 <jameskim2020@u.northwestern.edu>
2023-01-07 21:29:27 -0600

    created nested links

--- generate.py
+++ generate.py
@@ -2,6 +2,9 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+for i in range(5):
+    for j in range(5):
+        for k in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[i,j,0.5 + k] , size=[(1 * (0.9 ** k)),(1 * (0.9 ** k)),(1 * (0.9 ** k))])
 
 pyrosim.End()
