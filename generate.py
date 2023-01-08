317bc7c4322fe25d58f5a663404e908269ba4c05
jameskim1031 <jameskim2020@u.northwestern.edu>
2023-01-07 21:55:05 -0600

    Working on joint (torso_leg)

--- generate.py
+++ generate.py
@@ -1,10 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[0,5,0.5] , size=[1,1,1])
+    pyrosim.End()
 
-for i in range(5):
-    for j in range(5):
-        for k in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[i,j,0.5 + k] , size=[(1 * (0.9 ** k)),(1 * (0.9 ** k)),(1 * (0.9 ** k))])
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="Leg", pos=[1,0,1.5] , size=[1,1,1])
+    pyrosim.End()
 
-pyrosim.End()
+Create_Robot()
+Create_World()
