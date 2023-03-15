3ea4c5cc1e43c13246d1c70c422f90671dd1d825
jameskim1031 <jameskim2020@u.northwestern.edu>
2023-03-14 23:53:12 -0500

    final submission

--- search.py
+++ search.py
@@ -18,7 +18,7 @@ for i in range(c.numOfSeeds):
 # Plotting Fitness
 for i in range(c.numOfSeeds):
     plt.plot(fitness_curves[i], label='Random Seed ' + str(i))
-plt.title('Highest fitness vs Generations for' + str(c.numOfSeeds) + 'Random Seeds')
+plt.title('Highest fitness vs Generations for ' + str(c.numOfSeeds) + ' Random Seeds')
 plt.xlabel("Number of Generations")
 plt.ylabel("Highest fitness")
 plt.legend(loc = "upper left")
